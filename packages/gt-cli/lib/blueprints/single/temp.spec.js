81d5cebdc5d9c8cc07280523a41345fa8fe4de06
bookmarkbao <61926298+bookmarkbao@users.noreply.github.com>
2021-11-08 09:27:27 +0800

    add change

--- packages/gt-cli/lib/blueprints/single/temp.spec.js
+++ packages/gt-cli/lib/blueprints/single/temp.spec.js
@@ -1,28 +1,28 @@
 import Vue from 'vue';
-import IndexComponent from './index.vue';
+import {{name | camelCase}}Component from './index.vue';
 
 // Here are some Jasmine 2.0 tests, though you can
 // use any test runner / assertion library combo you prefer
-describe('IndexComponent', () => {
+describe('{{name | camelCase}}Component', () => {
   // Inspect the raw component options
   it('has a created hook', () => {
-    // expect(typeof IndexComponent.created).toBe('function');
+    // expect(typeof {{name | camelCase}}Component.created).toBe('function');
   })
   // Evaluate the results of functions in
   // the raw component options
   it('sets the correct default data', () => {
-    // expect(typeof IndexComponent.data).toBe('function')
-    // const defaultData = IndexComponent.data();
+    // expect(typeof {{name | camelCase}}Component.data).toBe('function')
+    // const defaultData = {{name | camelCase}}Component.data();
     // expect(defaultData.message).toBe('hello!');
   })
   // Inspect the component instance on mount
   it('correctly sets the message when created', () => {
-    // const vm = new Vue(IndexComponent).$mount();
+    // const vm = new Vue({{name | camelCase}}Component).$mount();
     // expect(vm.message).toBe('bye!');
   })
   // Mount an instance and inspect the render output
   it('renders the correct message', () => {
-    // const Ctor = Vue.extend(IndexComponent);
+    // const Ctor = Vue.extend({{name | camelCase}}Component);
     // const vm = new Ctor().$mount();
     // expect(vm.$el.textContent).toBe('bye!');
   })
